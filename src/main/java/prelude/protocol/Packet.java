3caa32f15a8ba6359120438b800adfe21e638e24
Preva1l <149850944+ProdPreva1l@users.noreply.github.com>
2024-06-19 12:04:32 +1000

    Update Packet.java

--- src/main/java/prelude/protocol/Packet.java
+++ src/main/java/prelude/protocol/Packet.java
@@ -8,4 +8,5 @@ public abstract class Packet<E> {
     public abstract boolean equals(Object packet);
     public abstract void loadData(InputStream is) throws InvalidPacketException;
     public abstract void processSelf(E handler);
+    public abstract UUID getRecipientUUID();
 }
